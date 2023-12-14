219efa9dacb384c7d7f554e225f6d07530268071
avanesyans <sergey.avanesyan@dowjones.com>
2023-12-14 14:04:35 -0500

    chore: remove logs

--- atm.js
+++ atm.js
@@ -1,40 +1,39 @@
 function ATM(money, limit) {
-	const nominalArr = ['50', '100', '500', '1000', '5000'];
-	const nominalsAmount = {};
-	const nlen = nominalArr.length;
-	for (let i = 0; i < nlen; i++) {
-		nominalsAmount[nominalArr[i]] = 0;
-	}
+  const nominalArr = ['50', '100', '500', '1000', '5000'];
+  const nominalsAmount = {};
+  const nlen = nominalArr.length;
+  for (let i = 0; i < nlen; i++) {
+    nominalsAmount[nominalArr[i]] = 0;
+  }
 
-	if (money % nominalArr[0] !== 0) { 
-		throw new Error("Incorrect amount of money.");
-	}
+  if (money % nominalArr[0] !== 0) {
+    throw new Error("Incorrect amount of money.");
+  }
 
-	let right = nlen - 1;
-	while (money && right >= 0) {
-		console.log('money: ', money);
-		const current = nominalArr[right];
-		if (limit[current] && money >= parseInt(current)) {
-			money -= parseInt(current);
-			nominalsAmount[current]++;
-			limit[current]--;
-		} else {
-			right--;
-		}
-	}
+  let right = nlen - 1;
+  while (money && right >= 0) {
+    const current = nominalArr[right];
+    if (limit[current] && money >= parseInt(current)) {
+      money -= parseInt(current);
+      nominalsAmount[current]++;
+      limit[current]--;
+    } else {
+      right--;
+    }
+  }
 
-	if (money > 0) { 
-		throw new Error("ATM does not have enough money.");
-	}
-	
-	return nominalsAmount;
+  if (money > 0) {
+    throw new Error("ATM does not have enough money.");
+  }
+
+  return nominalsAmount;
 }
 
 const limit = {
-	'5000' : 2,
-	'1000' : 2,
-	'500' : 3,
-	'100' : 2,
-	'50' : 10
+  '5000': 2,
+  '1000': 2,
+  '500': 3,
+  '100': 2,
+  '50': 10
 };
-console.log(ATM(7720, limit));
+console.log(ATM(7750, limit));
